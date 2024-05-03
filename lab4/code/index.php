231cd6963544dfa8f69a40a788816fa9f6744a3e
Yani <s.dambaef@gmail.com>
2024-05-03 11:24:16 +0200

    add index.php and save.php

--- lab4/code/index.php
+++ lab4/code/index.php
@@ -1,1 +1,43 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>doc</title>
+</head>
+<body>
+<div id="form">
+    <form action="save.php" method="post">
+        <label for="email">Email</label>
+        <input type="email" name="email" required>
+        <label for="category">Category</label>
+        <select name="category" required>
+            <option value="Laptops">Laptops</option>
+            <option value="Phones">Phones</option>
+        </select>
+
+        <label for="title">Title</label>
+        <input type="text" name="title" required>
+
+        <label for="description">Description</label>
+        <textarea rows="1" name="description"></textarea>
+
+        <input type="submit" value="save">
+    </form>
+</div>
+<div id="table">
+    <table>
+        <thead>
+        <th>Email</th>
+        <th>category</th>
+        <th>Title</th>
+        <th>Description</th>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+</body>
+</html>
---
+++ lab4/code/save.php
@@ -0,0 +1,58 @@
+<?php
+require_once 'vendor/autoload.php';
+$client = new Google_Client();
+$client->setAuthConfig('client_secret_72560971802-vv4tepuq78mmo7vpt615527hogq2cfao.apps.googleusercontent.com.json');
+$client->setScopes([Google_Service_Sheets::SPREADSHEETS]);
+
+$service = new Google_Service_Sheets($client);
+
+$spreadsheetId = '16I9UnQSKHgN0_X8Y-dU_a8J7OXJGQMOyx7MLlLpkrww';
+$range = 'A1:D';
+
+$email = $_POST['email'];
+$category = $_POST['category'];
+$title = $_POST['title'];
+$description = $_POST['description'];
+
+$values = [
+    [$email, $category, $title, $description],
+];
+
+$body = new Google_Service_Sheets_ValueRange([
+    'values' => $values
+]);
+
+$params = [
+    'valueInputOption' => 'RAW'
+];
+
+$result = $service->spreadsheets_values->append(
+    $spreadsheetId,
+    $range,
+    $body,
+    $params
+);
+
+if ($result) {
+    echo 'Данные успешно добавлены в таблицу!!!';
+} else {
+    echo 'Error 404';
+}
+
+$response = $service->spreadsheets_values->get($spreadsheetId, $range);
+$values = $response->getValues();
+
+// Отображение данных на странице
+if (!empty($values)) {
+    echo "<table border='1'>";
+    foreach ($values as $row) {
+        echo "<tr>";
+        foreach ($row as $cell) {
+            echo "<td>$cell</td>";
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "Данных нету";
+}
